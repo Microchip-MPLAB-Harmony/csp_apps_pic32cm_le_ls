16539200944f55d19be37bf60e405ea64426f11e
Jignesh Moradiya <jignesh.moradiya@microchip.com>
2020-06-12 01:52:24 +0530

    [APPS] Added trustZone_eic_eic_pin_toggle_pic32cm_ls00_cpro demo
    MH3-36964

---
+++ apps/trustZone/eic/eic_pin_toggle/Secure/firmware/src/main.c
@@ -0,0 +1,78 @@
+/*******************************************************************************
+  Main Source File
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    main.c
+
+  Summary:
+    This file contains the "main" function for a project.
+
+  Description:
+    This file contains the "main" function for a project.  The
+    "main" function calls the "SYS_Initialize" function to initialize the state
+    machines of all modules in the system
+ *******************************************************************************/
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+
+#include <stddef.h>                     // Defines NULL
+#include <stdbool.h>                    // Defines true
+#include <stdlib.h>                     // Defines EXIT_FAILURE
+#include "definitions.h"                // SYS function prototypes
+
+/* typedef for non-secure callback functions */
+typedef void (*funcptr_void) (void) __attribute__((cmse_nonsecure_call));
+
+void SWITCH_handler(uintptr_t context)
+{
+	LED_SECURE_Toggle();
+}
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Main Entry Point
+// *****************************************************************************
+// *****************************************************************************
+
+int main ( void )
+{
+    uint32_t msp_ns = *((uint32_t *)(TZ_START_NS));
+    volatile funcptr_void NonSecure_ResetHandler;
+
+    /* Initialize all modules */
+    SYS_Initialize ( NULL );
+
+    EIC_CallbackRegister(EIC_PIN_12, SWITCH_handler,  (uintptr_t) NULL);
+
+    if (msp_ns != 0xFFFFFFFF)
+    {
+        /* Set non-secure main stack (MSP_NS) */
+        __TZ_set_MSP_NS(msp_ns);
+
+        /* Get non-secure reset handler */
+        NonSecure_ResetHandler = (funcptr_void)(*((uint32_t *)((TZ_START_NS) + 4U)));
+
+        /* Start non-secure state software application */
+        NonSecure_ResetHandler();
+    }
+
+    while ( true )
+    {
+    }
+
+    /* Execution should not come here during normal operation */
+    return ( EXIT_FAILURE );
+}
+
+
+/*******************************************************************************
+ End of File
+*/
+
