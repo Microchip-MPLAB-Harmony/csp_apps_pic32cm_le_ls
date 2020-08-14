aaea44df7b4d2972e0620018ec489db71797ad5b
Jignesh Moradiya <jignesh.moradiya@microchip.com>
2020-08-14 10:10:53 +0530

    [APPS] Regenerated demo for trustZone_trustZone_basic_pic32cm_ls00_cpro
    MH3-40736 MH3-40738

--- apps/trustZone/trustZone_basic/Secure/firmware/src/packs/PIC32CM5164LS00100_DFP/pic32cm5164ls00100.h
+++ apps/trustZone/trustZone_basic/Secure/firmware/src/packs/PIC32CM5164LS00100_DFP/pic32cm5164ls00100.h
@@ -20,7 +20,7 @@
  *
  */
 
-/* file generated from device description version 2020-06-09T10:48:45Z */
+/* file generated from device description version 2020-08-03T13:09:29Z */
 #ifndef _PIC32CM5164LS00100_H_
 #define _PIC32CM5164LS00100_H_
 
@@ -30,12 +30,12 @@
 #define HEADER_FORMAT_VERSION_MAJOR (2)
 #define HEADER_FORMAT_VERSION_MINOR (0)
 
-/** \addtogroup PIC32CM5164LS00100_definitions PIC32CM5164LS00100 definitions
+/** \addtogroup PIC32CM5164LS00100_definitions b'PIC32CM5164LS00100 definitions
   This file defines all structures and symbols for PIC32CM5164LS00100:
     - registers and bitfields
     - peripheral base address
     - peripheral ID
-    - PIO definitions
+    - PIO definitions'
  *  @{
  */
 
@@ -65,7 +65,7 @@
 #  define _UL_(x) x   /**< Assembler: Unsigned Long integer literal constant value */
 #endif /* !(defined(__ASSEMBLER__) || defined(__IAR_SYSTEMS_ASM__)) */
 #endif /* SKIP_INTEGER_LITERALS */
-/** @}  end of Atmel Global Defines */
+/** @}  b'end of Atmel Global Defines' */
 
 /* ************************************************************************** */
 /*   CMSIS DEFINITIONS FOR PIC32CM5164LS00100                                 */
@@ -87,7 +87,7 @@ typedef enum IRQn
   SUPC_IRQn                 =   0, /**< 0   Shared between MCLK OSCCTRL OSC32KCTRL PM SUPC (SUPC) */
   MCLK_IRQn                 =   0, /**< 0   Shared between MCLK OSCCTRL OSC32KCTRL PM SUPC (MCLK) */
   PM_IRQn                   =   0, /**< 0   Shared between MCLK OSCCTRL OSC32KCTRL PM SUPC (PM) */
-  WDT_EW_IRQn               =   1, /**< 1   Watchdog Timer (WDT)                */
+  WDT_IRQn                  =   1, /**< 1   Watchdog Timer (WDT)                */
   RTC_IRQn                  =   2, /**< 2   Real-Time Counter (RTC)             */
   EIC_EXTINT_0_IRQn         =   3, /**< 3   External Interrupt Controller (EIC) */
   EIC_EXTINT_1_IRQn         =   4, /**< 4   External Interrupt Controller (EIC) */
@@ -98,9 +98,9 @@ typedef enum IRQn
   EIC_EXTINT_6_IRQn         =   9, /**< 9   External Interrupt Controller (EIC) */
   EIC_EXTINT_7_IRQn         =  10, /**< 10  External Interrupt Controller (EIC) */
   EIC_OTHER_IRQn            =  11, /**< 11  External Interrupt Controller (EIC) */
-  FREQM_DONE_IRQn           =  12, /**< 12  Frequency Meter (FREQM)             */
+  FREQM_IRQn                =  12, /**< 12  Frequency Meter (FREQM)             */
   NVMCTRL_IRQn              =  13, /**< 13  Non-Volatile Memory Controller (NVMCTRL) */
-  PORT_NSCHK_IRQn           =  14, /**< 14  Port Module (PORT)                  */
+  PORT_IRQn                 =  14, /**< 14  Port Module (PORT)                  */
   DMAC_0_IRQn               =  15, /**< 15  Direct Memory Access Controller (DMAC) */
   DMAC_1_IRQn               =  16, /**< 16  Direct Memory Access Controller (DMAC) */
   DMAC_2_IRQn               =  17, /**< 17  Direct Memory Access Controller (DMAC) */
@@ -116,7 +116,7 @@ typedef enum IRQn
   EVSYS_6_IRQn              =  27, /**< 27  Event System Interface (EVSYS)      */
   EVSYS_7_IRQn              =  28, /**< 28  Event System Interface (EVSYS)      */
   EVSYS_NSCHK_IRQn          =  29, /**< 29  Event System Interface (EVSYS)      */
-  PAC_ERR_IRQn              =  30, /**< 30  Peripheral Access Controller (PAC)  */
+  PAC_IRQn                  =  30, /**< 30  Peripheral Access Controller (PAC)  */
   SERCOM0_0_IRQn            =  31, /**< 31  Serial Communication Interface (SERCOM0) */
   SERCOM0_1_IRQn            =  32, /**< 32  Serial Communication Interface (SERCOM0) */
   SERCOM0_2_IRQn            =  33, /**< 33  Serial Communication Interface (SERCOM0) */
@@ -154,7 +154,7 @@ typedef enum IRQn
   DAC_UNDERRUN_IRQn         =  65, /**< 65  Digital-to-Analog Converter (DAC)   */
   DAC_EMPTY_IRQn            =  66, /**< 66  Digital-to-Analog Converter (DAC)   */
   PTC_IRQn                  =  67, /**< 67  Peripheral Touch Controller (PTC)   */
-  TRNG_DATARDY_IRQn         =  68, /**< 68  True Random Generator (TRNG)        */
+  TRNG_IRQn                 =  68, /**< 68  True Random Generator (TRNG)        */
   I2S_IRQn                  =  69, /**< 69  Inter-IC Sound Interface (I2S)      */
   TRAM_IRQn                 =  70, /**< 70  TrustRAM (TRAM)                     */
 
@@ -167,7 +167,7 @@ typedef struct _DeviceVectors
 {
   /* Stack pointer */
   void* pvStack;
-  /* Cortex-M handlers */
+  /* CORTEX-M23 handlers */
   void* pfnReset_Handler;                        /* -15 Reset Vector, invoked on Power up and warm reset */
   void* pfnNonMaskableInt_Handler;               /* -14 Non maskable Interrupt, cannot be stopped or preempted */
   void* pfnHardFault_Handler;                    /* -13 Hard Fault, all classes of Fault */
@@ -185,8 +185,8 @@ typedef struct _DeviceVectors
   void* pfnSysTick_Handler;                      /*  -1 System Tick Timer */
 
   /* Peripheral handlers */
-  void* pfnSYSTEM_Handler;                       /*   0  (MCLK OSCCTRL OSC32KCTRL PM SUPC) */
-  void* pfnWDT_EW_Handler;                       /*   1 Watchdog Timer (WDT) */
+  void* pfnSYSTEM_Handler;                       /*   0 System peripherals shared interrupt (MCLK OSCCTRL OSC32KCTRL PM SUPC) */
+  void* pfnWDT_Handler;                          /*   1 Watchdog Timer (WDT) */
   void* pfnRTC_Handler;                          /*   2 Real-Time Counter (RTC) */
   void* pfnEIC_EXTINT_0_Handler;                 /*   3 External Interrupt Controller (EIC) */
   void* pfnEIC_EXTINT_1_Handler;                 /*   4 External Interrupt Controller (EIC) */
@@ -197,9 +197,9 @@ typedef struct _DeviceVectors
   void* pfnEIC_EXTINT_6_Handler;                 /*   9 External Interrupt Controller (EIC) */
   void* pfnEIC_EXTINT_7_Handler;                 /*  10 External Interrupt Controller (EIC) */
   void* pfnEIC_OTHER_Handler;                    /*  11 External Interrupt Controller (EIC) */
-  void* pfnFREQM_DONE_Handler;                   /*  12 Frequency Meter (FREQM) */
+  void* pfnFREQM_Handler;                        /*  12 Frequency Meter (FREQM) */
   void* pfnNVMCTRL_Handler;                      /*  13 Non-Volatile Memory Controller (NVMCTRL) */
-  void* pfnPORT_NSCHK_Handler;                   /*  14 Port Module (PORT) */
+  void* pfnPORT_Handler;                         /*  14 Port Module (PORT) */
   void* pfnDMAC_0_Handler;                       /*  15 Direct Memory Access Controller (DMAC) */
   void* pfnDMAC_1_Handler;                       /*  16 Direct Memory Access Controller (DMAC) */
   void* pfnDMAC_2_Handler;                       /*  17 Direct Memory Access Controller (DMAC) */
@@ -215,7 +215,7 @@ typedef struct _DeviceVectors
   void* pfnEVSYS_6_Handler;                      /*  27 Event System Interface (EVSYS) */
   void* pfnEVSYS_7_Handler;                      /*  28 Event System Interface (EVSYS) */
   void* pfnEVSYS_NSCHK_Handler;                  /*  29 Event System Interface (EVSYS) */
-  void* pfnPAC_ERR_Handler;                      /*  30 Peripheral Access Controller (PAC) */
+  void* pfnPAC_Handler;                          /*  30 Peripheral Access Controller (PAC) */
   void* pfnSERCOM0_0_Handler;                    /*  31 Serial Communication Interface (SERCOM0) */
   void* pfnSERCOM0_1_Handler;                    /*  32 Serial Communication Interface (SERCOM0) */
   void* pfnSERCOM0_2_Handler;                    /*  33 Serial Communication Interface (SERCOM0) */
@@ -253,16 +253,10 @@ typedef struct _DeviceVectors
   void* pfnDAC_UNDERRUN_Handler;                 /*  65 Digital-to-Analog Converter (DAC) */
   void* pfnDAC_EMPTY_Handler;                    /*  66 Digital-to-Analog Converter (DAC) */
   void* pfnPTC_Handler;                          /*  67 Peripheral Touch Controller (PTC) */
-  void* pfnTRNG_DATARDY_Handler;                 /*  68 True Random Generator (TRNG) */
+  void* pfnTRNG_Handler;                         /*  68 True Random Generator (TRNG) */
   void* pfnI2S_Handler;                          /*  69 Inter-IC Sound Interface (I2S) */
   void* pfnTRAM_Handler;                         /*  70 TrustRAM (TRAM) */
 } DeviceVectors;
-
-/* Defines for Deprecated Interrupt and Exceptions handler names */
-#define pfnMemManage_Handler      pfnMemoryManagement_Handler     /**< \deprecated  Backward compatibility for ASF*/
-#define pfnDebugMon_Handler       pfnDebugMonitor_Handler         /**< \deprecated  Backward compatibility for ASF*/
-#define pfnNMI_Handler            pfnNonMaskableInt_Handler       /**< \deprecated  Backward compatibility for ASF*/
-#define pfnSVC_Handler            pfnSVCall_Handler               /**< \deprecated  Backward compatibility for ASF*/
 
 #endif /* !(defined(__ASSEMBLER__) || defined(__IAR_SYSTEMS_ASM__)) */
 
@@ -280,7 +274,7 @@ void SysTick_Handler               ( void );
 #if !defined DONT_USE_PREDEFINED_PERIPHERALS_HANDLERS
 /* Peripherals interrupt handlers */
 void SYSTEM_Handler                ( void );
-void WDT_EW_Handler                ( void );
+void WDT_Handler                   ( void );
 void RTC_Handler                   ( void );
 void EIC_EXTINT_0_Handler          ( void );
 void EIC_EXTINT_1_Handler          ( void );
@@ -291,9 +285,9 @@ void EIC_EXTINT_5_Handler          ( void );
 void EIC_EXTINT_6_Handler          ( void );
 void EIC_EXTINT_7_Handler          ( void );
 void EIC_OTHER_Handler             ( void );
-void FREQM_DONE_Handler            ( void );
+void FREQM_Handler                 ( void );
 void NVMCTRL_Handler               ( void );
-void PORT_NSCHK_Handler            ( void );
+void PORT_Handler                  ( void );
 void DMAC_0_Handler                ( void );
 void DMAC_1_Handler                ( void );
 void DMAC_2_Handler                ( void );
@@ -309,7 +303,7 @@ void EVSYS_5_Handler               ( void );
 void EVSYS_6_Handler               ( void );
 void EVSYS_7_Handler               ( void );
 void EVSYS_NSCHK_Handler           ( void );
-void PAC_ERR_Handler               ( void );
+void PAC_Handler                   ( void );
 void SERCOM0_0_Handler             ( void );
 void SERCOM0_1_Handler             ( void );
 void SERCOM0_2_Handler             ( void );
@@ -347,21 +341,13 @@ void AC_Handler                    ( void );
 void DAC_UNDERRUN_Handler          ( void );
 void DAC_EMPTY_Handler             ( void );
 void PTC_Handler                   ( void );
-void TRNG_DATARDY_Handler          ( void );
+void TRNG_Handler                  ( void );
 void I2S_Handler                   ( void );
 void TRAM_Handler                  ( void );
 #endif /* DONT_USE_PREDEFINED_PERIPHERALS_HANDLERS */
-/* Defines for Deprecated Interrupt and Exceptions handler names */
-#define MemManage_Handler         MemoryManagement_Handler        /**< \deprecated  Backward compatibility*/
-#define DebugMon_Handler          DebugMonitor_Handler            /**< \deprecated  Backward compatibility*/
-#define NMI_Handler               NonMaskableInt_Handler          /**< \deprecated  Backward compatibility*/
-#define SVC_Handler               SVCall_Handler                  /**< \deprecated  Backward compatibility*/
-
 #endif /* !(defined(__ASSEMBLER__) || defined(__IAR_SYSTEMS_ASM__)) */
 
-/*
- * \brief Configuration of the CORTEX-M23 Processor and Core Peripherals
- */
+/** \brief Configuration of the CORTEX-M23 Processor and Core Peripherals */
 #define __FPU_PRESENT                  0 /**< FPU present or not                                                        */
 #define __MPU_PRESENT                  1 /**< MPU present or not                                                        */
 #define __NVIC_PRIO_BITS               2 /**< Number of NVIC Priority bits                                              */
@@ -378,7 +364,7 @@ void TRAM_Handler                  ( void );
 #include "system_pic32cmls00.h"
 #endif /* USE_CMSIS_INIT */
 
-/** \defgroup PIC32CM5164LS00100_api Peripheral Software API
+/** \defgroup PIC32CM5164LS00100_api b'Peripheral Software API'
  *  @{
  */
 
@@ -416,9 +402,9 @@ void TRAM_Handler                  ( void );
 #include "component/trng.h"
 #include "component/usb.h"
 #include "component/wdt.h"
-/** @}  end of Peripheral Software API */
+/** @}  b'end of Peripheral Software API' */
 
-/** \addtogroup PIC32CM5164LS00100_id Peripheral Ids Definitions
+/** \addtogroup PIC32CM5164LS00100_id b'Peripheral Ids Definitions'
  *  @{
  */
 
@@ -468,9 +454,9 @@ void TRAM_Handler                  ( void );
 #define ID_TRAM          ( 85) /**< \brief Instance index for TRAM (TRAM) */
 
 #define ID_PERIPH_MAX    ( 85) /**< \brief Number of peripheral IDs */
-/** @}  end of Peripheral Ids Definitions */
+/** @}  b'end of Peripheral Ids Definitions' */
 
-/** \addtogroup PIC32CM5164LS00100_base Peripheral Base Address Definitions
+/** \addtogroup PIC32CM5164LS00100_base b'Peripheral Base Address Definitions'
  *  @{
  */
 
@@ -528,9 +514,9 @@ void TRAM_Handler                  ( void );
 #define USB_REGS                         ((usb_registers_t*)0x4100a000)                /**< \brief USB Registers Address        */
 #define WDT_REGS                         ((wdt_registers_t*)0x40002000)                /**< \brief WDT Registers Address        */
 #endif /* (defined(__ASSEMBLER__) || defined(__IAR_SYSTEMS_ASM__)) */
-/** @}  end of Peripheral Base Address Definitions */
+/** @}  b'end of Peripheral Base Address Definitions' */
 
-/** \addtogroup PIC32CM5164LS00100_base Peripheral Base Address Definitions
+/** \addtogroup PIC32CM5164LS00100_base b'Peripheral Base Address Definitions'
  *  @{
  */
 
@@ -586,9 +572,9 @@ void TRAM_Handler                  ( void );
 #define TRNG_BASE_ADDRESS                _UL_(0x42004400)                              /**< \brief TRNG Base Address */
 #define USB_BASE_ADDRESS                 _UL_(0x4100a000)                              /**< \brief USB Base Address */
 #define WDT_BASE_ADDRESS                 _UL_(0x40002000)                              /**< \brief WDT Base Address */
-/** @}  end of Peripheral Base Address Definitions */
+/** @}  b'end of Peripheral Base Address Definitions' */
 
-/** \addtogroup PIC32CM5164LS00100_pio Peripheral Pio Definitions
+/** \addtogroup PIC32CM5164LS00100_pio b'Peripheral Pio Definitions'
  *  @{
  */
 
@@ -596,7 +582,7 @@ void TRAM_Handler                  ( void );
 /*   PIO DEFINITIONS FOR PIC32CM5164LS00100                                   */
 /* ************************************************************************** */
 #include "pio/pic32cm5164ls00100.h"
-/** @}  end of Peripheral Pio Definitions */
+/** @}  b'end of Peripheral Pio Definitions' */
 
 /* ************************************************************************** */
 /*   MEMORY MAPPING DEFINITIONS FOR PIC32CM5164LS00100                        */
@@ -856,7 +842,7 @@ void TRAM_Handler                  ( void );
 }
 #endif
 
-/** @}  end of PIC32CM5164LS00100 definitions */
+/** @}  b'end of PIC32CM5164LS00100 definitions' */
 
 
 #endif /* _PIC32CM5164LS00100_H_ */
