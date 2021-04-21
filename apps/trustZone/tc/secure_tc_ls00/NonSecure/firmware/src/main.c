af731279c227a439641d27b89f92902b7d787926
Dimitri DAMBAS <dimitri.dambas@microchip.com>
2021-04-21 16:10:08 +0200

    [TRUSTZONE/TC] Integrated PIC32CM LS00 Secure TC example
    MH3-51729

---
+++ apps/trustZone/tc/secure_tc_ls00/NonSecure/firmware/src/main.c
@@ -0,0 +1,115 @@
+/*******************************************************************************
+  Main Source File
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    main.c
+
+  Summary:
+    This file contains the "main" function for a project.
+
+  Description:
+    This file contains the "main" function for a project.  The
+    "main" function calls the "SYS_Initialize" function to initialize the state
+    machines of all modules in the system
+ *******************************************************************************/
+
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+* Copyright (C) 2021 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ *******************************************************************************/
+// DOM-IGNORE-END
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+
+#include <stddef.h>                     // Defines NULL
+#include <stdbool.h>                    // Defines true
+#include <stdlib.h>                     // Defines EXIT_FAILURE
+#include "definitions.h"                // SYS function prototypes
+
+#include "trustZone/nonsecure_entry.h"
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Data Input
+// *****************************************************************************
+// ****************************************************************************
+#define delay_ms 10
+
+void LED0_On (void)
+{
+    LED0_Set ();
+}
+
+void LED0_Off (void)
+{
+    LED0_Clear ();
+}
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Main Entry Point
+// *****************************************************************************
+// *****************************************************************************
+
+int main ( void )
+{
+    volatile uint8_t duty_cycle;
+    
+    /* Initialize all modules */
+    SYS_Initialize ( NULL );
+    
+    /* Set the TC0 interrupt callback registers */
+    nsc_tc0_match0_interrupt_callback_register ( LED0_On );
+    nsc_tc0_overflow_interrupt_callback_register ( LED0_Off );
+    
+    //Start SysTick timer
+    nsc_SYSTICK_TimerStart ();
+    // Enable TC0 peripheral
+    nsc_TC0_CompareStart ();
+
+    while ( true )
+    {
+        for (duty_cycle = 0xFF ; duty_cycle >= 0x10 ; duty_cycle--)
+        {
+            // Set Secure timer counter 0 PWM duty cycle
+            nsc_TC0_DutyCycle_Set (duty_cycle);
+            nsc_SYSTICK_DelayMs ( delay_ms );
+        }
+    }
+
+    /* Execution should not come here during normal operation */
+
+    return ( EXIT_FAILURE );
+}
+
+
+/*******************************************************************************
+ End of File
+*/
+
