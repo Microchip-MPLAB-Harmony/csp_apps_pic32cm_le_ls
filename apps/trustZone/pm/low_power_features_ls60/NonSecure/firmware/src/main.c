6193429824a4e0d4c8d6a00a53155586720130b0
Dimitri DAMBAS <dimitri.dambas@microchip.com>
2021-04-21 11:14:33 +0200

    [TRUSTZONE/PM] Integrated PIC32CM LS60 Low-Power Features example
    MH3-51728

---
+++ apps/trustZone/pm/low_power_features_ls60/NonSecure/firmware/src/main.c
@@ -0,0 +1,259 @@
+/*******************************************************************************
+  Main Source File
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    main.c
+
+  Summary:
+    This file contains the "main" function for a project.
+
+  Description:
+    This file contains the "main" function for a project.  The
+    "main" function calls the "SYS_Initialize" function to initialize the state
+    machines of all modules in the system
+ *******************************************************************************/
+
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+* Copyright (C) 2021 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+*******************************************************************************/
+// DOM-IGNORE-END
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+
+#include <stddef.h>                     // Defines NULL
+#include <stdbool.h>                    // Defines true
+#include <stdlib.h>                     // Defines EXIT_FAILURE
+#include "definitions.h"                // SYS function prototypes
+
+#include "trustZone/nonsecure_entry.h"
+
+/* Maximum number of DMA transfer allowed */
+#define MAX_DMA_TRANSFER 10
+#define DMAC_BITCOUNT_VALUE 10
+
+SECTION_DMAC_DESCRIPTOR volatile uint16_t adc_result_store;
+
+/* Application Modes' Definitions*/
+typedef enum
+{
+    IDLE_MODE_IRQ,
+    STANDBY_MODE_IRQ,
+    STANDBY_MODE_SLPWKG,
+    OFF_MODE
+}APP_MODES;
+
+extern void Light_sensor_to_data_streamer ( void );
+        
+// Global 8-bit array for the DGI communication
+uint8_t DataStream_buf[4] = {0x03, 0x00, 0x00, 0xFC};
+
+// Global variables for DMA descriptors
+uint32_t srcBuffer = (uint32_t)(&ADC_REGS->ADC_RESULT);
+uint32_t destBuffer = (uint32_t)(&adc_result_store) + (DMAC_BITCOUNT_VALUE * (DMAC_BTCTRL_STEPSIZE_X1_Val + 1));
+
+APP_MODES app_current_mode = IDLE_MODE_IRQ;
+
+// Global Variables
+volatile bool DMAC_Flag = false;
+volatile bool SW0_Flag = false;
+volatile bool read_light_sensor = true;
+volatile bool led0_state = false;
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Function Definitions
+// *****************************************************************************
+// *****************************************************************************
+
+// *** DMAC Functions *** //
+// Program enters this Interrupt Handler when DMAC transfer is completed
+static void TransmitCompleteCallback (DMAC_TRANSFER_EVENT event, uintptr_t contextHandle)
+{
+    DMAC_Flag = true;
+}
+
+// Program enters this Interrupt Handler when on-board switch button is pressed
+static void ButtonPressedCallback (uintptr_t context)
+{
+    SW0_Flag = true;
+}
+
+// *** EIC Functions *** //
+// Program enters this Interrupt Handler when RTC counter reach 0xCCC
+static void RTCCountFinished (RTC_TIMER32_INT_MASK intCause, uintptr_t context)
+{
+    ADC_ConversionStart (); 
+}
+
+// *** Application Functions *** //
+// Collect light sensor results, make an average and sent it to the data streamer
+void Light_sensor_to_data_streamer ( void )
+{
+    uint16_t average = 0;
+    uint32_t light = 0;
+    uint32_t light2 = 0;
+
+    // Global 8-bit array for the DGI communication
+    DataStream_buf[1] = 0;
+    DataStream_buf[2] = 0;
+
+    // Light sensor data streamer formating data
+    for (uint8_t i = 0; i < (MAX_DMA_TRANSFER / 2); i++)
+    {
+        light = *(unsigned short*)(&adc_result_store + (i * 2));
+        light2 = light + light2;
+    }
+
+    average = light2 / (MAX_DMA_TRANSFER / 2);
+
+    // Send the average value of the last 10 ADC results
+    DataStream_buf[1] = (uint8_t)average;
+    DataStream_buf[2] = (uint8_t)(average>>8);
+    
+    // Send data through the SPI enabled channel
+    SERCOM4_SPI_Write (DataStream_buf, 4);
+}
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Main Entry Point
+// *****************************************************************************
+// *****************************************************************************
+
+// *** Non-Secure Main Function *** //
+int main ( void )
+{
+    // Initialize all modules
+    SYS_Initialize ( NULL );
+    
+    DMAC_ChannelCallbackRegister(DMAC_CHANNEL_0, TransmitCompleteCallback, 0);
+    EIC_CallbackRegister (EIC_PIN_12, ButtonPressedCallback, 0);
+    RTC_Timer32CallbackRegister (RTCCountFinished, 0);
+    
+    // Enable ADC peripheral
+    ADC_Enable ();
+    
+    // Enable RTC peripheral
+    RTC_Timer32Start ();
+    
+    //Replace with your SECURE application code
+    while (1) 
+    {
+        // Start DMA Channel Transfer
+        DMAC_ChannelTransfer (DMAC_CHANNEL_0, (const void*)srcBuffer, (const void*)destBuffer, 2*DMAC_BITCOUNT_VALUE );
+        
+		if (SW0_Flag)
+        {
+            SW0_Flag = false;
+            if (app_current_mode == OFF_MODE)
+            {
+                app_current_mode = IDLE_MODE_IRQ;
+            }
+            else
+            {
+                app_current_mode++;
+            }
+        }
+
+        switch (app_current_mode)
+        {
+            case IDLE_MODE_IRQ:
+                // Unset DMA Flag
+                DMAC_Flag = false;
+
+                while (!DMAC_Flag)
+                {
+                    // Enter in IDLE Sleep Mode
+                    nsc_pm_idle_mode_enter ();
+                }
+                
+                // DGI Transfer to the Data Streamer
+                Light_sensor_to_data_streamer ();
+                break;
+
+            case STANDBY_MODE_IRQ:
+                // Unset DMA Flag
+                DMAC_Flag = false;
+
+                while (!DMAC_Flag)
+                {
+                    // Enter in STANDBY Sleep Mode
+                    nsc_pm_standby_mode_enter ();
+                }
+
+                // DGI Transfer to the Data Streamer
+                Light_sensor_to_data_streamer ();
+                break;
+
+            case STANDBY_MODE_SLPWKG:
+                // Unset DMA Flag
+                DMAC_Flag = false;
+                
+                // Disable RTC_IRQ for Sleepwalking
+                RTC_Timer32InterruptDisable (RTC_TIMER32_INT_MASK_CMP0);
+                NVIC_DisableIRQ (RTC_IRQn);      
+                
+                // Initialize Event System for SleepWalking
+                nsc_evsys_initialize ();
+
+                while (!DMAC_Flag)
+                {
+                    // Enter in IDLE Sleep Mode
+                    nsc_pm_standby_mode_enter ();
+                }
+
+                // DGI Transfer to the Data Streamer
+                Light_sensor_to_data_streamer ();
+                break;
+
+            case OFF_MODE:
+                // Unset DMA_Flag
+                DMAC_Flag = false;
+                
+                // Disable ADC peripheral
+                ADC_Disable ();
+                
+                // Disable RTC peripheral
+                RTC_Timer32Stop ();
+
+                // Enter in OFF Sleep Mode
+                nsc_pm_off_mode_enter ();
+                break;
+        }
+    }
+
+    /* Execution should not come here during normal operation */
+    return ( EXIT_FAILURE );
+}
+
+
+/*******************************************************************************
+ End of File
+*/
---
+++ apps/trustZone/pm/low_power_features_ls60/readme.md
@@ -0,0 +1,102 @@
+---
+parent: Harmony 3 peripheral library application examples for PIC32CM LE00/LS00/LS60 family
+title: Low-Power Features 
+has_children: false
+has_toc: false
+---
+
+[![MCHP](https://www.microchip.com/ResourcePackages/Microchip/assets/dist/images/logo.png)](https://www.microchip.com)
+
+# Low-Power Features
+
+This example highlights the power consumption for each sleep mode based on the same application integration.
+
+## Description
+
+This example application configures RTC to generate an interrupt or event (regarding sleep mode and configuration) to start an ADC conversion from light sensor value every 1 ms. The DMAC move the converted value from ADC Result to internal SRAM and after 10 transfers, DMA generates an interrupt to wake-up core and process data through DGI SPI pins. Application initial sleep mode state is IDLE mode. Pressing the SW0 button allows to go from one sleep mode to another.
+
+Below is the low-power configuration for this application:
+- Voltage Regulator is a Buck Converter
+- Low Power Efficiency Mode is enabled
+- Performance Level 2 (PL2) Mode is selected
+- Power Domain Switchable (PDSW) is always ACTIVE
+
+## Downloading and building the application
+
+To clone or download this application from Github, go to the [main page of this repository](https://github.com/Microchip-MPLAB-Harmony/csp_apps_pic32cm_le_ls) and then click **Clone** button to clone this repository or download as zip file.
+This content can also be downloaded using content manager by following these [instructions](https://github.com/Microchip-MPLAB-Harmony/contentmanager/wiki).
+
+Path of the application within the repository is as shown below:
+
+| Type        | Path                         |
+|:-----------:|:----------------------------:|
+| Project group | apps/trustZone/low_power_features_ls60 |
+|Secure Project|  apps/trustZone/low_power_features_ls60/Secure/firmware |
+|Non-Secure Project|  apps/trustZone/low_power_features_ls60/NonSecure/firmware |
+||||
+
+To build the application, refer to the following table and open the project using its IDE.
+
+| Project Name      | Description                                    |
+| ----------------- | ---------------------------------------------- |
+| pic32cm_ls60_cpro.X <br> pic32cm_ls60_cpro_NonSecure.X | Secure and Non-Secure MPLABX projects for MPLABX project for [PIC32CM LS60 Curiosity Pro Evaluation Kit]() |
+|||
+
+## Setting up the hardware
+
+The following table shows the target hardware for the application projects.
+
+| Project Name| Board|
+|:---------|:---------:|
+| pic32cm_ls60_cpro.X <br> pic32cm_ls60_cpro_NonSecure.X | [PIC32CM LS60 Curiosity Pro Evaluation Kit]() |
+| I/O1 Xplained Pro Extension Kit | [I/O1 Xplained Pro Extension Kit](https://www.microchip.com/DevelopmentTools/ProductDetails/ATIO1-XPRO) |
+|||
+
+### Setting up [PIC32CM LS60 Curiosity Pro Evaluation Kit]()
+
+- Plug the I/O1 Xplained Pro Extension Kit to the EXT1 header on the board
+- Connect the Debug USB port on the board to the computer using a micro USB cable
+
+## Running the Application
+
+1. Build and Program the application using its IDE
+2. Open and Configure the [Microchip's Atmel Data Visualizer](https://gallery.microchip.com/packages/AtmelDataVisualizerInstaller-Standalone/)
+	- Open a Data Streamer in Configuration > Protocols
+	
+	![output](images/data_streamer_protocol.png)
+	
+	- Configure and Load the Data Streamer with the DataStream_Configuration text file available in NonSecure > firmware > src
+	
+	![output](images/data_stream_control_panel.png)
+	
+	- Open a Graph in Configuration > Visualization
+	
+	![output](images/graph_visualizer.png)
+	
+	- Drag & Drop the LightLevel icon to Graph window as a New plot
+	
+	![output](images/routed_light_level.png)
+	
+	- Connect the board with DGI Control Panel
+	
+	![output](images/dgi_control_panel_connect.png)
+	
+	- Select SPI & Power intefaces
+	
+	![output](images/dgi_control_panel_interfaces.png)
+	
+	- Drag & Drop the SPI icon to Data Stream Control Panel
+	
+	![output](images/routed_dgi_spi_output.png)
+	
+	- Start the communication with the board
+	
+	![output](images/dgi_control_panel_start.png)
+
+3. Power consumption and SPI Output should appear on the screen
+
+	![output](images/spi_output_details.png)
+	
+	![output](images/power_consumption_idle_mode.png)
+	
+4. Push SW0 button to go from one sleep mode to another (IDLE, STANDBY with IRQ, Static SleepWalking, OFF). Push RESET button when the device is in OFF mode to restart code example at initial sleep state (IDLE)
