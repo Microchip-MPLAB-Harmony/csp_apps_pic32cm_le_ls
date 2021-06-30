682a95200014c8690e15718b9acc800b0e42ae87
Dimitri Dambas <dimitri.dambas@microchip.com>
2021-06-30 16:20:09 +0200

    [TRUSTZONE/NVMCTRL] Integrated the PIC32CM LE00 Data Flash with Silent Access and RTC Tamper Detection example
    MH3-53109

---
+++ apps/nvmctrl/nvmctrl_dataflash_SA_TE/firmware/src/main.c
@@ -0,0 +1,176 @@
+/*******************************************************************************
+  Main Source File
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    main.c
+
+  Summary:
+    This file contains the "main" function for a project.
+
+  Description:
+    This file contains the "main" function for a project.  The
+    "main" function calls the "SYS_Initialize" function to initialize the state
+    machines of all modules in the system
+ *******************************************************************************/
+
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+* Copyright (C) 2021 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ *******************************************************************************/
+// DOM-IGNORE-END
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+
+#include <stddef.h>                     // Defines NULL
+#include <stdbool.h>                    // Defines true
+#include <stdlib.h>                     // Defines EXIT_FAILURE
+#include "definitions.h"                // SYS function prototypes
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Data Inputs
+// *****************************************************************************
+// *****************************************************************************
+
+#define data_buffer_length 16
+uint32_t data_buffer[data_buffer_length];
+
+uint32_t* dataflash_addr_page0 = (uint32_t *) DATAFLASH_ADDR;
+uint32_t* dataflash_addr_page1 = (uint32_t *) (DATAFLASH_ADDR + 0x40);
+
+bool tamper_detected = false;
+
+typedef enum
+{
+    PAGE_NUMBER_0,
+    PAGE_NUMBER_1
+} PAGE_NUMBERS;
+
+bool display_dataflash_page_content (PAGE_NUMBERS page_number)
+{
+    switch (page_number)
+    {
+        case (PAGE_NUMBER_0):
+            for (uint32_t i = 0 ; i < 16 ; i++)
+            {
+                if (i%2 == 0)
+                    printf ("\n\r Page 0 : 0x%X : ", (uint)(dataflash_addr_page0+i));
+                
+                printf ("%08X ", (uint)data_buffer[i]);
+            }
+            return 0;
+            
+        case (PAGE_NUMBER_1):
+            for (uint32_t i = 0 ; i < 16 ; i++)
+            {
+                if (i%2 == 0)
+                    printf ("\n\r Page 0 : 0x%X : ", (uint)(dataflash_addr_page1+i));
+                
+                printf ("%08X ", (uint)data_buffer[i]);
+            }
+            return 0;
+            
+        default:
+            printf ("\n\r ! ERROR: Page number must be 0 or 1");
+            return 1;
+    }
+}
+
+void RTC_TamperDetected_Interrupt ( RTC_TIMER32_INT_MASK callback, uintptr_t context )
+{
+    if (callback & RTC_TIMER32_INT_MASK_TAMPER)
+    {
+        printf ("\n\n\r - RTC Tamper Detected !");
+        printf ("\n\n\r - Press RESET button to wakeup the board\n\r");
+        PM_OffModeEnter();
+    }
+}
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Main Entry Point
+// *****************************************************************************
+// *****************************************************************************
+
+int main ( void )
+{
+    /* Initialize all modules */
+    SYS_Initialize ( NULL );
+    
+    SYSTICK_TimerStart ();
+    RTC_Timer32CallbackRegister (RTC_TamperDetected_Interrupt, 0);
+    RTC_Timer32Start ();
+    
+    printf ("\33[H\33[2J");
+    printf ("\n\n\r##########################################");
+	printf ("\n\r#        Secure DataFlash example        #");
+	printf ("\n\r##########################################");
+    
+    // Erase TEROW
+    printf ("\n\n\r - Erase TEROW");
+    // Write pattern in TEROW
+    printf ("\n\n\r - Write 0xCAFEDECA pattern in TEROW");
+    
+    NVMCTRL_RowErase ((uint32_t)dataflash_addr_page0);    
+    
+    // Prepare page pattern for the TEROW
+    for (uint32_t i = 0; i < data_buffer_length; i++)
+        data_buffer[i] = 0xCAFEDECA;
+        
+    // Write data_buffer's content in TEROW Page 0
+    NVMCTRL_PageWrite (data_buffer, (uint32_t)dataflash_addr_page0);
+    // Write data_buffer's contentTEROW Page 1
+    NVMCTRL_PageWrite (data_buffer, (uint32_t)dataflash_addr_page1);
+
+    while ( true )
+    {
+        printf ("\n\n\r - Print TEROW Page 0 & Page 1 content :");
+        // Read TEROW Page 0 content
+        NVMCTRL_Read (data_buffer, 64, (uint32_t)dataflash_addr_page0);
+        // Display TEROW Page 0 content
+        display_dataflash_page_content (PAGE_NUMBER_0);
+
+        // Read TEROW Page 1 content
+        NVMCTRL_Read (data_buffer, 64, (uint32_t)dataflash_addr_page1);
+        // Display TEROW Page 1 content
+        display_dataflash_page_content (PAGE_NUMBER_1);
+
+        // Initiate a 1s delay using SysTick timer
+        SYSTICK_DelayMs (1000);
+    }
+
+    /* Execution should not come here during normal operation */
+    return ( EXIT_FAILURE );
+}
+
+
+/*******************************************************************************
+ End of File
+*/
+
---
+++ apps/nvmctrl/nvmctrl_dataflash_SA_TE/readme.md
@@ -0,0 +1,66 @@
+---
+parent: Harmony 3 peripheral library application examples for PIC32CM LE00/LS00/LS60 family
+title: Data Flash with Silent Access and RTC Tamper Detection
+has_children: false
+has_toc: false
+---
+
+[![MCHP](https://www.microchip.com/ResourcePackages/Microchip/assets/dist/images/logo.png)](https://www.microchip.com)
+
+# Data Flash with Silent Access and RTC Tamper Detection
+
+This example application displays the Data Flash content on a console which is refreshed every seconds. The Silent Access and Tamper Erase security features of the Data Flash are enabled.
+
+## Description
+
+This application configures the RTC peripheral for tamper detection, the SERCOM 3 peripheral to display content on console through UART pins. The application enable Data Flash security features (Silent Access and Tamper Erase), erase and display Data Flash content, fill Data Flash and display its content on console. When a Tampere is detected, Non-Secure application is halted and Data Flash content is fully erased.
+
+
+## Downloading and building the application
+
+To clone or download this application from Github, go to the [main page of this repository](https://github.com/Microchip-MPLAB-Harmony/csp_apps_pic32cm_le_ls) and then click **Clone** button to clone this repository or download as zip file.
+This content can also be downloaded using content manager by following these [instructions](https://github.com/Microchip-MPLAB-Harmony/contentmanager/wiki).
+
+Path of the application within the repository is **apps/nvmctrl/nvmctrl_dataflash_SA_TE/firmware** .
+||||
+
+To build the application, refer to the following table and open the project using its IDE.
+
+| Project Name      | Description                                    |
+| ----------------- | ---------------------------------------------- |
+| pic32cm_le00_curiosity_pro.X | MPLABX project for [PIC32CM LE00 Curiosity Pro Evaluation Kit]() |
+|||
+
+## Setting up the hardware
+
+The following table shows the target hardware for the application projects.
+
+| Project Name| Board|
+|:---------|:---------:|
+| pic32cm_le00_curiosity_pro.X | [PIC32CM LE00 Curiosity Pro Evaluation Kit]()
+|||
+
+### Setting up [PIC32CM LE00 Curiosity Pro Evaluation Kit]()
+
+- Connect the Debug USB port on the board to the computer using a micro USB cable
+
+
+## Running the Application
+
+1. Open the Terminal application (Ex.:Tera term) on the computer
+2. Connect to the EDBG Virtual COM port and configure the serial settings as follows:
+    - Baud : 115200
+    - Data : 8 Bits
+    - Parity : None
+    - Stop : 1 Bit
+    - Flow Control : None
+3. Open the project group in the MPLAB X IDE and set the non-secure project as main project
+4. Build and program the application
+5. The console should display the following Secure Data Flash content:
+
+![output](images/output_dataflash_example.png)
+
+6. Tied down the PA08 pin with a jumper
+7. The following Secure Data Flash content should be observed on console, meaning the Secure Data Flash Tamper Erase Row 0 was performed because of RTC Tamper Detection:
+
+![output](images/output_dataflash_rtc_tamper_detected.png)
