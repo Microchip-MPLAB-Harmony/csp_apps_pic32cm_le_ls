ef7a8ed8c9532a8704c4e89da586993cb7b12629
Jignesh Moradiya <jignesh.moradiya@microchip.com>
2021-08-04 10:48:22 +0530

    [SERCOM] Regenerated demo for sercom_spi_spi_eeprom_write_read_pic32cm_le00_curiosity_pro
    MH3-56858
    
    Signed-off-by: Jignesh Moradiya <jignesh.moradiya@microchip.com>

--- apps/sercom/spi/spi_eeprom_write_read/firmware/src/config/pic32cm_le00_curiosity_pro/peripheral/port/plib_port.c
+++ apps/sercom/spi/spi_eeprom_write_read/firmware/src/config/pic32cm_le00_curiosity_pro/peripheral/port/plib_port.c
@@ -74,15 +74,18 @@ void PORT_Initialize(void)
    /************************** GROUP 0 Initialization *************************/
    PORT_REGS->GROUP[0].PORT_DIR = 0x400;
    PORT_REGS->GROUP[0].PORT_OUT = 0x400;
+   PORT_REGS->GROUP[0].PORT_PINCFG[10] = 0x0;
 
    PORT_REGS->GROUP[0].PORT_PMUX[5] = 0x20;
 
    /************************** GROUP 1 Initialization *************************/
    PORT_REGS->GROUP[1].PORT_DIR = 0x101;
    PORT_REGS->GROUP[1].PORT_OUT = 0x101;
+   PORT_REGS->GROUP[1].PORT_PINCFG[0] = 0x0;
    PORT_REGS->GROUP[1].PORT_PINCFG[1] = 0x1;
    PORT_REGS->GROUP[1].PORT_PINCFG[2] = 0x1;
    PORT_REGS->GROUP[1].PORT_PINCFG[3] = 0x1;
+   PORT_REGS->GROUP[1].PORT_PINCFG[8] = 0x0;
 
    PORT_REGS->GROUP[1].PORT_PMUX[0] = 0x20;
    PORT_REGS->GROUP[1].PORT_PMUX[1] = 0x22;
@@ -90,6 +93,7 @@ void PORT_Initialize(void)
    /************************** GROUP 2 Initialization *************************/
    PORT_REGS->GROUP[2].PORT_DIR = 0x80000;
    PORT_REGS->GROUP[2].PORT_OUT = 0x80000;
+   PORT_REGS->GROUP[2].PORT_PINCFG[19] = 0x0;
 
 
 }
@@ -303,7 +307,7 @@ void PORT_PinPeripheralFunctionConfig(PORT_PIN pin, PERIPHERAL_FUNCTION function
     PORT_GROUP group = GET_PORT_GROUP(pin);
     uint32_t pin_num = ((uint32_t)pin) & 0x1FU;
     uint32_t pinmux_val = (uint32_t)((port_group_registers_t*)group)->PORT_PMUX[(pin_num >> 1)];
-    
+
     /* For odd pins */
     if (0U != (pin_num & 0x01U))
     {
@@ -314,7 +318,7 @@ void PORT_PinPeripheralFunctionConfig(PORT_PIN pin, PERIPHERAL_FUNCTION function
         pinmux_val = (pinmux_val & ~0x0FU) | periph_func;
     }
     ((port_group_registers_t*)group)->PORT_PMUX[(pin_num >> 1)] = (uint8_t)pinmux_val;
-    
+
     /* Enable peripheral control of the pin */
     ((port_group_registers_t*)group)->PORT_PINCFG[pin_num] |= (uint8_t)PORT_PINCFG_PMUXEN_Msk;
 }
