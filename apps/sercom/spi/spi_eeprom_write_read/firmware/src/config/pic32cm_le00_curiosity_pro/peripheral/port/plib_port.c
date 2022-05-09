e2a3a249ec897fc0a3b59150a7fdb06d237f61be
Jignesh Moradiya <jignesh.moradiya@microchip.com>
2022-05-09 11:54:30 +0530

    [SERCOM] Regenerated demo for sercom_spi_spi_eeprom_write_read_pic32cm_le00_curiosity_pro
    MH3-65792
    
    Signed-off-by: Jignesh Moradiya <jignesh.moradiya@microchip.com>

--- apps/sercom/spi/spi_eeprom_write_read/firmware/src/config/pic32cm_le00_curiosity_pro/peripheral/port/plib_port.c
+++ apps/sercom/spi/spi_eeprom_write_read/firmware/src/config/pic32cm_le00_curiosity_pro/peripheral/port/plib_port.c
@@ -52,7 +52,6 @@
 // Section: PORT Implementation
 // *****************************************************************************
 // *****************************************************************************
-
 // *****************************************************************************
 /* Function:
 
@@ -89,15 +88,17 @@ void PORT_Initialize(void)
 
    PORT_REGS->GROUP[1].PORT_PMUX[0] = 0x20;
    PORT_REGS->GROUP[1].PORT_PMUX[1] = 0x22;
+   PORT_REGS->GROUP[1].PORT_PMUX[4] = 0x0;
 
    /************************** GROUP 2 Initialization *************************/
    PORT_REGS->GROUP[2].PORT_DIR = 0x80000;
    PORT_REGS->GROUP[2].PORT_OUT = 0x80000;
    PORT_REGS->GROUP[2].PORT_PINCFG[19] = 0x0;
 
+   PORT_REGS->GROUP[2].PORT_PMUX[9] = 0x0;
+
 
 }
-
 // *****************************************************************************
 /* Function:
     uint32_t PORT_GroupRead(PORT_GROUP group)
