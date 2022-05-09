61bfa98aa70974267b24930d529d0d05138e9291
Jignesh Moradiya <jignesh.moradiya@microchip.com>
2022-05-09 12:46:33 +0530

    [TRUSTZONE] Regenerated demo for trustZone_evsys_evsys_trigger_ls00_NonSecure_pic32cm_ls00_cpro
    MH3-65810
    
    Signed-off-by: Jignesh Moradiya <jignesh.moradiya@microchip.com>

--- apps/trustZone/evsys/evsys_trigger_ls00/Secure/firmware/src/config/pic32cm_ls00_cpro/peripheral/port/plib_port.c
+++ apps/trustZone/evsys/evsys_trigger_ls00/Secure/firmware/src/config/pic32cm_ls00_cpro/peripheral/port/plib_port.c
@@ -52,7 +52,6 @@
 // Section: PORT Implementation
 // *****************************************************************************
 // *****************************************************************************
-
 // *****************************************************************************
 /* Function:
 
@@ -77,6 +76,7 @@ void PORT_Initialize(void)
    PORT_SEC_REGS->GROUP[0].PORT_PINCFG[22] = 0x0;
    PORT_SEC_REGS->GROUP[0].PORT_PINCFG[23] = 0x0;
 
+   PORT_SEC_REGS->GROUP[0].PORT_PMUX[11] = 0x0;
    PORT_SEC_REGS->GROUP[0].PORT_EVCTRL = 0xf700f6;
 
    PORT_SEC_REGS->GROUP[0].PORT_NONSEC = 0x800000;
@@ -92,11 +92,15 @@ void PORT_Initialize(void)
    PORT_SEC_REGS->GROUP[2].PORT_PINCFG[27] = 0x0;
    PORT_SEC_REGS->GROUP[2].PORT_PINCFG[28] = 0x5;
 
+   PORT_SEC_REGS->GROUP[2].PORT_PMUX[9] = 0x0;
+   PORT_SEC_REGS->GROUP[2].PORT_PMUX[10] = 0x0;
+   PORT_SEC_REGS->GROUP[2].PORT_PMUX[13] = 0x0;
+   PORT_SEC_REGS->GROUP[2].PORT_PMUX[14] = 0x0;
    PORT_SEC_REGS->GROUP[2].PORT_EVCTRL = 0xfb00f300L;
 
    PORT_SEC_REGS->GROUP[2].PORT_NONSEC = 0x180000;
-}
 
+}
 // *****************************************************************************
 /* Function:
     uint32_t PORT_GroupRead(PORT_GROUP group)
