123af678a492ab08571bcfb09fc395c9ab94dc2c
Jignesh Moradiya <Jignesh.Moradiya@microchip.com>
2023-04-26 01:09:42 +0530

    [TRAM] Regenerated demo for tram_tram_tamper_erase_pic32cm_le00_curiosity_pro
    MH3-77380

--- apps/tram/tram_tamper_erase/firmware/src/config/pic32cm_le00_curiosity_pro/peripheral/port/plib_port.c
+++ apps/tram/tram_tamper_erase/firmware/src/config/pic32cm_le00_curiosity_pro/peripheral/port/plib_port.c
@@ -71,21 +71,21 @@
 void PORT_Initialize(void)
 {
    /************************** GROUP 0 Initialization *************************/
-   PORT_REGS->GROUP[0].PORT_PINCFG[8] = 0x1;
+   PORT_REGS->GROUP[0].PORT_PINCFG[8] = 0x1U;
 
-   PORT_REGS->GROUP[0].PORT_PMUX[4] = 0x6;
+   PORT_REGS->GROUP[0].PORT_PMUX[4] = 0x6U;
 
    /************************** GROUP 1 Initialization *************************/
-   PORT_REGS->GROUP[1].PORT_PINCFG[20] = 0x1;
-   PORT_REGS->GROUP[1].PORT_PINCFG[21] = 0x1;
+   PORT_REGS->GROUP[1].PORT_PINCFG[20] = 0x1U;
+   PORT_REGS->GROUP[1].PORT_PINCFG[21] = 0x1U;
 
-   PORT_REGS->GROUP[1].PORT_PMUX[10] = 0x22;
+   PORT_REGS->GROUP[1].PORT_PMUX[10] = 0x22U;
 
    /************************** GROUP 2 Initialization *************************/
-   PORT_REGS->GROUP[2].PORT_DIR = 0x80000;
-   PORT_REGS->GROUP[2].PORT_PINCFG[19] = 0x0;
+   PORT_REGS->GROUP[2].PORT_DIR = 0x80000U;
+   PORT_REGS->GROUP[2].PORT_PINCFG[19] = 0x0U;
 
-   PORT_REGS->GROUP[2].PORT_PMUX[9] = 0x0;
+   PORT_REGS->GROUP[2].PORT_PMUX[9] = 0x0U;
 
 
 }
