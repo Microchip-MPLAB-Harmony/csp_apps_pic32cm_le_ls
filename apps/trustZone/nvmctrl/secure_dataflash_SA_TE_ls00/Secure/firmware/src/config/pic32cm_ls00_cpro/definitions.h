361774be9b890bb2dc5df1c529be03ffcf13c4cb
Jignesh Moradiya <Jignesh.Moradiya@microchip.com>
2023-04-28 16:04:17 +0530

    [NVMCTRL] Regenerated and tested trustZone_nvmctrl_secure_dataflash_SA_TE_ls00_pic32cm_ls00_cpro demo
    MH3-77395, MH3-77396

--- apps/trustZone/nvmctrl/secure_dataflash_SA_TE_ls00/Secure/firmware/src/config/pic32cm_ls00_cpro/definitions.h
+++ apps/trustZone/nvmctrl/secure_dataflash_SA_TE_ls00/Secure/firmware/src/config/pic32cm_ls00_cpro/definitions.h
@@ -52,10 +52,10 @@
 #include "peripheral/sercom/usart/plib_sercom3_usart.h"
 #include "peripheral/nvmctrl/plib_nvmctrl.h"
 #include "peripheral/evsys/plib_evsys.h"
-#include "peripheral/pm/plib_pm.h"
 #include "peripheral/port/plib_port.h"
 #include "peripheral/clock/plib_clock.h"
 #include "peripheral/nvic/plib_nvic.h"
+#include "peripheral/pm/plib_pm.h"
 #include "peripheral/rtc/plib_rtc.h"
 // DOM-IGNORE-BEGIN
 #ifdef __cplusplus  // Provide C++ Compatibility
@@ -67,6 +67,12 @@ extern "C" {
 
 /* Non-secure start address */
 #define TZ_START_NS 0x40000
+
+/* Device Information */
+#define DEVICE_NAME			 "PIC32CM5164LS00100"
+#define DEVICE_ARCH			 "CORTEX-M23"
+#define DEVICE_FAMILY		 "PIC32CM"
+#define DEVICE_SERIES		 "PIC32CMLS00"
 
 /* CPU clock frequency */
 #define CPU_CLOCK_FREQUENCY 48000000
