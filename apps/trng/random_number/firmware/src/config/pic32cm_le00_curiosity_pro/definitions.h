d961a2134c61ddf695061af88f1a4637b34334ba
Jignesh Moradiya <Jignesh.Moradiya@microchip.com>
2024-10-21 18:46:06 +0530

    [TRNG] Regenerated demo for trng__random_number__firmware__pic32cm_le00_curiosity_pro.X
    MH3-99812

--- apps/trng/random_number/firmware/src/config/pic32cm_le00_curiosity_pro/definitions.h
+++ apps/trng/random_number/firmware/src/config/pic32cm_le00_curiosity_pro/definitions.h
@@ -55,8 +55,8 @@
 #include "peripheral/port/plib_port.h"
 #include "peripheral/clock/plib_clock.h"
 #include "peripheral/nvic/plib_nvic.h"
-#include "peripheral/pm/plib_pm.h"
 #include "peripheral/trng/plib_trng.h"
+#include "peripheral/pm/plib_pm.h"
 #include "peripheral/eic/plib_eic.h"
 
 // DOM-IGNORE-BEGIN
@@ -68,13 +68,13 @@ extern "C" {
 // DOM-IGNORE-END
 
 /* Device Information */
-#define DEVICE_NAME			 "PIC32CM5164LE00100"
-#define DEVICE_ARCH			 "CORTEX-M23"
-#define DEVICE_FAMILY		 "PIC32CM"
-#define DEVICE_SERIES		 "PIC32CMLE00"
+#define DEVICE_NAME          "PIC32CM5164LE00100"
+#define DEVICE_ARCH          "CORTEX-M23"
+#define DEVICE_FAMILY        "PIC32CM"
+#define DEVICE_SERIES        "PIC32CMLE00"
 
 /* CPU clock frequency */
-#define CPU_CLOCK_FREQUENCY 48000000
+#define CPU_CLOCK_FREQUENCY 48000000U
 
 // *****************************************************************************
 // *****************************************************************************
