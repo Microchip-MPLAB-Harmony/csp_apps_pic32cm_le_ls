2ba8923b49a1b80660e4e478559643a7fe5d7d9f
Jignesh Moradiya <Jignesh.Moradiya@microchip.com>
2024-10-24 17:37:58 +0530

    [APPS] Regenerated trustZone_trustZone_basic_ls60_pic32cm_ls60_cpro demo
    MH3-99864

--- apps/trustZone/trustZone_basic_ls60/NonSecure/firmware/src/config/pic32cm_ls60_cpro/definitions.h
+++ apps/trustZone/trustZone_basic_ls60/NonSecure/firmware/src/config/pic32cm_ls60_cpro/definitions.h
@@ -62,13 +62,13 @@ extern "C" {
 // DOM-IGNORE-END
 
 /* Device Information */
-#define DEVICE_NAME			 "PIC32CM5164LS60100"
-#define DEVICE_ARCH			 "CORTEX-M23"
-#define DEVICE_FAMILY		 "PIC32CM"
-#define DEVICE_SERIES		 "PIC32CMLS60"
+#define DEVICE_NAME          "PIC32CM5164LS60100"
+#define DEVICE_ARCH          "CORTEX-M23"
+#define DEVICE_FAMILY        "PIC32CM"
+#define DEVICE_SERIES        "PIC32CMLS60"
 
 /* CPU clock frequency */
-#define CPU_CLOCK_FREQUENCY 48000000
+#define CPU_CLOCK_FREQUENCY 48000000U
 
 // *****************************************************************************
 // *****************************************************************************
