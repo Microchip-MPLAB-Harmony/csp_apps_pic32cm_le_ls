e0d49999d4bd4f8140b04feaf55d40fb797de582
Syam Nair <Syam.Nair@microchip.com>
2025-09-03 14:18:47 +0530

    [APP] Regenerated demo for trustZone__evsys__evsys_trigger_ls60
    MH3C-264

--- apps/trustZone/evsys/evsys_trigger_ls60/NonSecure/firmware/src/config/pic32cm_ls60_cpro/interrupts.c
+++ apps/trustZone/evsys/evsys_trigger_ls60/NonSecure/firmware/src/config/pic32cm_ls60_cpro/interrupts.c
@@ -20,7 +20,7 @@
 
 // DOM-IGNORE-BEGIN
 /*******************************************************************************
-* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+* Copyright (C) 2025 Microchip Technology Inc. and its subsidiaries.
 *
 * Subject to your compliance with these terms, you may use Microchip software
 * and any derivatives exclusively with Microchip products. It is your
@@ -80,11 +80,11 @@ void __attribute__((optimize("-O1"), long_call, noreturn, used))Dummy_Handler(vo
 /* MISRAC 2012 deviation block start */
 /* MISRA C-2012 Rule 8.6 deviated 5 times.  Deviation record ID -  H3_MISRAC_2012_R_8_6_DR_1 */
 /* Device vectors list dummy definition*/
-extern void SVCall_Handler             ( void ) __attribute__((weak, alias("Dummy_Handler")));
-extern void PendSV_Handler             ( void ) __attribute__((weak, alias("Dummy_Handler")));
-extern void SysTick_Handler            ( void ) __attribute__((weak, alias("Dummy_Handler")));
-extern void EIC_OTHER_Handler          ( void ) __attribute__((weak, alias("Dummy_Handler")));
-extern void EVSYS_1_Handler            ( void ) __attribute__((weak, alias("Dummy_Handler")));
+extern void SVCall_Handler             ( void ) __attribute__((weak, alias("Dummy_Handler"),noreturn));
+extern void PendSV_Handler             ( void ) __attribute__((weak, alias("Dummy_Handler"),noreturn));
+extern void SysTick_Handler            ( void ) __attribute__((weak, alias("Dummy_Handler"),noreturn));
+extern void EIC_OTHER_Handler          ( void ) __attribute__((weak, alias("Dummy_Handler"),noreturn));
+extern void EVSYS_1_Handler            ( void ) __attribute__((weak, alias("Dummy_Handler"),noreturn));
 
 
 /* MISRAC 2012 deviation block end */
