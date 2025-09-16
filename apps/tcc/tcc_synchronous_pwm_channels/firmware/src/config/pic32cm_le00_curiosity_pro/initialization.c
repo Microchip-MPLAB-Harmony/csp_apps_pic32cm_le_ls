b6b91ec93e8aaaea971ab7ece0296b3037e55755
Syam Nair <Syam.Nair@microchip.com>
2025-09-16 17:59:44 +0000

    [APP] Regenerated demo for tcc__tcc_synchronous_pwm_channels__firmware
    MH3C-264

--- apps/tcc/tcc_synchronous_pwm_channels/firmware/src/config/pic32cm_le00_curiosity_pro/initialization.c
+++ apps/tcc/tcc_synchronous_pwm_channels/firmware/src/config/pic32cm_le00_curiosity_pro/initialization.c
@@ -15,7 +15,7 @@
 
 // DOM-IGNORE-BEGIN
 /*******************************************************************************
-* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+* Copyright (C) 2025 Microchip Technology Inc. and its subsidiaries.
 *
 * Subject to your compliance with these terms, you may use Microchip software
 * and any derivatives exclusively with Microchip products. It is your
@@ -145,9 +145,9 @@ void SYS_Initialize ( void* data )
 
     NVMCTRL_Initialize();
 
-    EVSYS_Initialize();
-
     TCC0_PWMInitialize();
+
+    EVSYS_Initialize();
 
 
     NVIC_Initialize();
